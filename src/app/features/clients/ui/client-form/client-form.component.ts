82f4496b6f6e81ae4479d813ac5fa93091ec83c2
abr0xas <qqpp0090w@gmail.com>
2025-07-10 15:24:57 +0200

    Refactor client form HTML and TypeScript for improved readability and consistency
    - Updated input elements in client-form.component.html to use self-closing tags for better HTML standards.
    - Enhanced TypeScript code in client-form.component.ts by organizing imports and adding a test method for debugging purposes.
    - Ensured consistent formatting and added missing commas in object definitions.

--- src/app/features/clients/ui/client-form/client-form.component.ts
+++ src/app/features/clients/ui/client-form/client-form.component.ts
@@ -1,6 +1,11 @@
 import { Component, input, output, inject } from '@angular/core';
 import { RestaurantClient } from '../../data/models/client.modal';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
@@ -8,15 +13,15 @@ import { ChangeDetectionStrategy } from '@angular/core';
   imports: [ReactiveFormsModule],
   templateUrl: './client-form.component.html',
   styleUrl: './client-form.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ClientFormComponent {
   // Usar output() function en lugar de @Output decorator
   clientCreated = output<RestaurantClient>();
-  
+
   // Usar inject() function en lugar de constructor injection
   private fb = inject(FormBuilder);
-  
+
   form = this.createRestaurantClientForm();
 
   createRestaurantClientForm(): FormGroup {
@@ -26,19 +31,23 @@ export class ClientFormComponent {
       contact: this.fb.group({
         name: ['', Validators.required],
         email: ['', [Validators.required, Validators.email]],
-        phone: ['', Validators.required]
+        phone: ['', Validators.required],
       }),
       address: this.fb.group({
         street: ['', Validators.required],
         city: ['', Validators.required],
         country: ['', Validators.required],
-        postalCode: ['']
+        postalCode: [''],
       }),
       status: ['active', Validators.required],
       createdAt: [null],
       updatedAt: [null],
-      users: [[]]
+      users: [[]],
     });
+  }
+
+  testPrestier(any: any) {
+    console.log('test', any);
   }
 
   onSubmit() {
